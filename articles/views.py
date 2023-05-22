b912d8651e2c042fa2ff088c1ba791a1f1de467d
Bookwhale00 <Bookwhale00@gmail.com>
2023-05-22 21:15:44 +0900

    :memo: Feat: 게시글 CRUD 양식 작성 #2
    articles/views.py ArticleView CRUD 양식 추가

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class ArticleView(APIView):
+    def get(self, request, article_id):
+        """상세 게시글 보기"""
+        pass
+
+    def post(self, request):
+        """게시글 작성"""
+        pass
+
+    def put(self, request, article_id):
+        """게시글 수정"""
+        pass
+
+    def delete(self, request, article_id):
+        """게시글 삭제"""
+        pass
