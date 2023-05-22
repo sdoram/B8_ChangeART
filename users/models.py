2ca45415859856476a11e96c4131287f5ff16466
BenCyera <dltpgml1124@gmail.com>
2023-05-22 21:31:37 +0900

    ✨Feat:회원가입, 로그인기능 추가/유저 모델 추가 # 1
    settings.py : Access_tocen_lifetime의 시간을 임시 조정했습니다
    users/models.py : User 모델을 추가했습니다
    users/serializer.py : UserSerizlier 추가했습니다
    users/urls.py : 회원가입, 로그인의 url 추가했습니다
    users/views.py : 회원가입, 로그인 추가했습니다.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from articles.models import Article
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password=None):
+        user = self.create_user(email, password=password, nickname=nickname)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    nickname = models.CharField(max_length=50, verbose_name="닉네임")
+    profile_image = models.ImageField(null=True, blank=True, verbose_name="프로필사진")
+    following = models.ManyToManyField(
+        Article, blank=True, related_name="following_list", verbose_name="팔로잉 리스트"
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [nickname]
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
