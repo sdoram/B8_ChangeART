5a6135f1eeea673d5ae77d8c2feea55cb86feca8
Kim Kyungjin <grace8362@naver.com>
2023-05-24 17:07:38 +0900

    :bug:Fix: 누락된 쉼표 추가, 미사용 코드 제거 #7

--- users/views.py
+++ users/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
 from users.models import User
 from users.serializers import UserSerializer, UserTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
