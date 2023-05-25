b9c650e521f4f59d3b7c2973e45bcce05373ec38
BenCyera <dltpgml1124@gmail.com>
2023-05-25 09:27:40 +0900

    ✨Feat: MypageView 팔로잉리스트, 팔로잉&팔로워 count 추가
    users/views.py : Response의 메세지를 일부 수정했습니다
    users/serializers.py : MypageView에서 사용할 FollowListSerializer를 추가했습니다.
    			UserPageSerializer에 following, followers count를 추가했습니다.

--- users/serializers.py
+++ users/serializers.py
@@ -25,6 +25,31 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 
+# 인증번호 대조
+class VerifySerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    verify_code = serializers.CharField(max_length=6)
+
+    def validate(self, data):
+        email = data.get("email")
+        verify_code = data.get("verify_code")
+        access = get_object_or_404(Verify, email=email)
+        if verify_code == access.athnt_code:
+            return data
+
+
+# 마이페이지의 팔로잉 리스트
+class FollowListSerializer(serializers.ModelSerializer):
+    nickname = serializers.CharField()
+    profile_image = serializers.ImageField()
+
+    class Meta:
+        model = User
+        fields = ["nickname", "profile_image"]
+        # ordering = ["id"]
+
+
+# 마이페이지의 게시글
 class UserArticlesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
@@ -34,11 +59,32 @@ class UserArticlesSerializer(serializers.ModelSerializer):
 # 마이페이지용
 class UserPageSerializer(serializers.ModelSerializer):
     user_articles = UserArticlesSerializer(many=True, read_only=True)
-
+    following_list = FollowListSerializer(many=True, read_only=True)
+    following_count = serializers.SerializerMethodField()
+    followers_count = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ["email", "nickname", "last_login", "profile_image", "user_articles"]
+        fields = [
+            "email",
+            "nickname",
+            "last_login",
+            "profile_image",
+            "following_count",
+            "followers_count",
+            "following_list",
+            "user_articles",
+        ]
+
+    def get_following_count(self, obj):
+        following_count = User.objects.filter(following=obj).count()
+        return following_count
+
+    def get_followers_count(self, obj):
+        return obj.following.count()
+
+    def get_following_count(self, obj):
+        return obj.following_list.count()
 
 
 # 로그인용
--- users/views.py
+++ users/views.py
@@ -70,7 +70,6 @@ class MyPageView(APIView):
     # 내 정보 보기
     def get(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
-
         serializer = UserPageSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -93,7 +92,7 @@ class MyPageView(APIView):
         if request.user == user:
             user.is_active = False
             user.save()
-            return Response({"message": "탈퇴하셨습니다."}, status=status.HTTP_200_OK)
+            return Response({"message": "탈퇴하셨습니다"}, status=status.HTTP_200_OK)
         else:
             return Response({"message": "권한이 없습니다"}, status=status.HTTP_400_BAD_REQUEST)
          
@@ -106,7 +105,7 @@ class FollowView(APIView):
         user = request.user                                   # 나
         if user in following_user.following.all():
             following_user.following.remove(user)
-            return Response({"message": "팔로우 취소"}, status=status.HTTP_200_OK)
+            return Response({"message": "팔로우를 취소했습니다"}, status=status.HTTP_200_OK)
         else:
             following_user.following.add(user)
-            return Response({"message": "팔로우"}, status=status.HTTP_200_OK)
+            return Response({"message": "팔로우했습니다"}, status=status.HTTP_200_OK)
