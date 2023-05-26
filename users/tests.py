04a2fe8cc96727c9f6488796705194eb56ef7b1f
Seman_Kim <sdoramkim@gmail.com>
2023-05-26 14:21:18 +0900

    :art:Improve: setUpTestData 사용 #6
    users/test.py : classmethod를 활용한 setUpTestData로 중복실행 제거

--- users/tests.py
+++ users/tests.py
@@ -14,9 +14,12 @@ class AthntCodeCreateViewTest(APITestCase):
 
 
 class SignupViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.email = {"email": "signup@test.com"}
+        cls.url = reverse("athnt_code_create_view")
+
     def setUp(self):
-        self.email = {"email": "signup@test.com"}
-        self.url = reverse("athnt_code_create_view")
         self.response = self.client.post(self.url, self.email)
         self.verify = Verify.objects.get(email=self.email["email"])
 
@@ -43,13 +46,16 @@ class LoginViewTest(APITestCase):
 
 
 class MyPageViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user("login@test.com", "테스트", "1234")
+        cls.login_data = {"email": "login@test.com", "password": "1234"}
+        cls.put_data = {"nickname": "프로필 수정", "password": "change1234"}
+
     def setUp(self):
-        self.user = User.objects.create_user("login@test.com", "테스트", "1234")
-        self.login_data = {"email": "login@test.com", "password": "1234"}
         self.access_token = self.client.post(
             reverse("login_view"), self.login_data
         ).data["access"]
-        self.put_data = {"nickname": "프로필 수정", "password": "change1234"}
 
     def test_get_user(self):
         response = self.client.get(path=reverse("my_page_view", args=[self.user.id]))
@@ -72,11 +78,14 @@ class MyPageViewTest(APITestCase):
 
 
 class FollowViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.from_user = User.objects.create_user("following@test.com", "팔로잉", "1234")
+        cls.to_user = User.objects.create_user("follow@test.com", "팔로우", "1234")
+        cls.from_user_data = {"email": "following@test.com", "password": "1234"}
+        cls.to_user_data = {"email": "follow@test.com", "password": "1234"}
+
     def setUp(self):
-        self.from_user = User.objects.create_user("following@test.com", "팔로잉", "1234")
-        self.to_user = User.objects.create_user("follow@test.com", "팔로우", "1234")
-        self.from_user_data = {"email": "following@test.com", "password": "1234"}
-        self.to_user_data = {"email": "follow@test.com", "password": "1234"}
         self.from_user_access_token = self.client.post(
             reverse("login_view"), self.from_user_data
         ).data["access"]
