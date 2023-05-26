365b4f8b726a84d177aaba447f377e71db37d0d6
Seman_Kim <sdoramkim@gmail.com>
2023-05-26 11:59:56 +0900

    ✅:Test: 회원가입,로그인,이메일 인증 테스트 #1 #8
    users/tests.py : 이메일 인증 테스트 코드 작성

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import User, Verify
+
+
+# 인증코드 발송
+class AthntCodeCreateViewTest(APITestCase):
+    def test_send_success(self):
+        url = reverse("athnt_code_create_view")
+        data = {"email": "test@test.com"}
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+
+
+# 회원가입
+class SignupViewTest(APITestCase):
+    def setUp(self):
+        self.email = {"email": "signup@test.com"}
+        self.url = reverse("athnt_code_create_view")
+        self.response = self.client.post(self.url, self.email)
+        self.verify = Verify.objects.get(email=self.email["email"])
+
+    def test_signup_success(self):
+        url = reverse("signup_view")
+        user_data = {
+            "email": "signup@test.com",
+            "nickname": "테스트",
+            "password": "1234",
+            "verify_code": self.verify.athnt_code,
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.status_code, 201)
+
+
+# 로그인
+class LoginViewTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            "login@test.com",
+            "테스트",
+            "1234",
+        )
+        self.login_data = {
+            "email": "login@test.com",
+            "password": "1234",
+        }
+
+    def test_login(self):
+        response = self.client.post(reverse("login_view"), self.login_data)
+        self.assertEqual(response.status_code, 200)
